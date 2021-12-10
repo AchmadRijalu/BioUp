3d82717d9e21346da4520b6de4fa7eeabfcb65d6
AlvinGary <alvingary201@gmail.com>
2021-12-11 00:37:07 +0700

    add seeder level

--- database/seeders/LevelSeeder.php
+++ database/seeders/LevelSeeder.php
@@ -7,48 +7,76 @@ use Illuminate\Support\Facades\DB;
 
 class LevelSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('bio12_levels')->insert([
-            'levelID' => '1',
-            'charID' => '1'
-         ]);
-         DB::table('bio12_levels')->insert([
-            'levelID' => '2',
-            'charID' => '1'
-         ]);
-         DB::table('bio12_levels')->insert([
-            'levelID' => '3',
-            'charID' => '1'
-         ]);
-         DB::table('bio12_levels')->insert([
-            'levelID' => '4',
-            'charID' => '2'
-         ]);
-         DB::table('bio12_levels')->insert([
-            'levelID' => '5',
-            'charID' => '2'
-         ]);
-         DB::table('bio12_levels')->insert([
-            'levelID' => '6',
-            'charID' => '2'
-         ]);
-         DB::table('bio12_levels')->insert([
-            'levelID' => '7',
-            'charID' => '3'
-         ]);
-         DB::table('bio12_levels')->insert([
-            'levelID' => '8',
-            'charID' => '3'
-         ]);
-         DB::table('bio12_levels')->insert([
-            'levelID' => '9',
-            'charID' => '3'
-         ]);
-    }
+   /**
+    * Run the database seeds.
+    *
+    * @return void
+    */
+   public function run()
+   {
+      DB::table('bio12_levels')->insert([
+         'levelID' => '1',
+         'charID' => '1'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '2',
+         'charID' => '1'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '3',
+         'charID' => '1'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '4',
+         'charID' => '2'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '5',
+         'charID' => '2'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '6',
+         'charID' => '2'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '7',
+         'charID' => '3'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '8',
+         'charID' => '3'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '9',
+         'charID' => '3'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '10',
+         'charID' => '4'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '11',
+         'charID' => '4'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '12',
+         'charID' => '4'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '13',
+         'charID' => '5'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '14',
+         'charID' => '5'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '15',
+         'charID' => '5'
+      ]);
+      DB::table('bio12_levels')->insert([
+         'levelID' => '16',
+         'charID' => '6'
+      ]);
+   }
 }
