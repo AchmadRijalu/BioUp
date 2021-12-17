29ac4e736236c7f99e4c43c18cb08fe7b6b42c9f
Achmad Rijalu <achmad.rijalu@gmail.com>
2021-12-17 16:57:21 +0700

    New User Create User level

--- app/Http/Controllers/CharController.php
+++ app/Http/Controllers/CharController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Character;
+use App\Models\UserLevel;
 use Illuminate\Http\Request;
 use App\Models\UserCharacter;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class CharController extends Controller
@@ -16,6 +19,7 @@ class CharController extends Controller
      */
     public function index()
     {
+        $loop1 = 1;
         //
         if(UserCharacter::where('user_id', Auth::id())->first() == null){
             UserCharacter::create([
@@ -23,6 +27,17 @@ class CharController extends Controller
                 'character_id' => 1,
                 'score' => 0,
             ]);
+
+            for ($i = 0; $i < 3; $i++) {
+                DB::table('bio12_user_levels')->insert([
+                    'level_id' => $loop1++,
+                    'character_id' => 1,
+                    'user_id' => Auth::id(),
+                    'score' => 0,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ]);
+            }
         }
         $character = Character::all();
         $userchar = UserCharacter::where('user_id', Auth::id())
